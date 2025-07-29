c76022519af69d304bbebb4e24c53d6f75766e99
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-07-29 11:15:02 +0530

    Create NextPermutation.java

---
+++ Arrays/Arrays1DQuestions/NextPermutation.java
@@ -0,0 +1,25 @@
+    //Next Permutation
+    public void nextPermutation(int arr[]){
+        int n = arr.length;
+        int pivot = -1;
+        for(int i=n-2;i>=0;i--){
+           if(arr[i]<arr[i+1]){
+              pivot = i;
+              break;
+           }
+        }
+
+        if (pivot == -1) {
+            rev(arr, 0, n-1);
+            return;
+        }
+        for(int i=n-1;i>pivot;i++){
+            if (arr[i]>arr[pivot]) {
+                int temp = arr[i];
+                arr[i] = arr[pivot];
+                arr[pivot] = temp;
+                break;
+            }
+        }
+        rev(arr, pivot+1, n-1);
+    }
