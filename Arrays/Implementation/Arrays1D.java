162e6c7abb9bcbcab397bd554a83c732790f4abc
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-08-05 22:18:20 +0530

    Create Arrays1D.java

---
+++ Arrays/Implementation/Arrays1D.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Arrays1D{
+    public static void main(String[] args) {
+        
+        //1D Arrays 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter array elements");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+         System.out.println("Print array elements");
+         System.out.print("[");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("]");
+
+
+
+    }
+}
