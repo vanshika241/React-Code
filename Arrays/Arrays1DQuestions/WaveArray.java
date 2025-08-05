016820efda79c261c41350d2cdd8da9123309c6f
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-08-05 22:16:41 +0530

    Delete Arrays directory

--- Arrays/Arrays1DQuestions/WaveArray.java
+++
@@ -1,9 +0,0 @@
-    //Wave Array
-    public void sortInWave(int arr[]){
-        int n = arr.length;
-        for(int i=1;i<n;i+=2){
-            int temp = arr[i];
-            arr[i] = arr[i-1];
-            arr[i-1] = temp;
-        }
-    }
