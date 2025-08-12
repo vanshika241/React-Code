ebffb4d5a9a11f7447cc000c3275190c4a32a95c
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-08-12 19:44:18 +0530

    Create SearchPattern.java

---
+++ Strings/StringQuestions/SearchPattern.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int[] getLPS(String pat){
+        int n = pat.length();
+        int pre = 0;
+        int suff = 1;
+        int lps[] = new int[n];
+        lps[0] = 0;
+        while(suff<n){
+            if(pat.charAt(pre) == pat.charAt(suff)){
+                lps[suff] = pre+1;
+                pre++;
+                suff++;
+            }
+            else{
+                if(pre ==0){
+                    lps[suff] = 0;
+                    suff++;
+                }
+                else{
+                    pre = lps[pre-1];
+                }
+            }
+        }
+        return lps;
+        
+    }
+    ArrayList<Integer> search(String pat, String txt) {
+        // code here
+        ArrayList<Integer>  ans = new ArrayList<>();
+        
+        int lps[] = getLPS(pat);
+        int f = 0;
+        int s = 0;
+        int n = txt.length();
+        int m = pat.length();
+        while(f<n){
+            if(txt.charAt(f) == pat.charAt(s) ){
+                s++;
+                f++;
+                if(s == m){
+                    ans.add(f-m);
+                    s = lps[s-1];
+                }
+            }
+            else{
+                if(s == 0){
+                    f++;
+                }
+                else{
+                    s = lps[s-1];
+                }
+            }
+        }
+        return ans;
+    } 
+}
